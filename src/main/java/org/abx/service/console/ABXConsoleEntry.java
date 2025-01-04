1e25de3d43192f1dc475501996593310a41689e5
Luis Carlos Lara <l3clara@gmail.com>
2025-01-03 22:47:00 -0600

    test

--- src/main/java/org/abx/service/console/ABXConsoleEntry.java
+++ src/main/java/org/abx/service/console/ABXConsoleEntry.java
@@ -2,11 +2,15 @@ package org.abx.service.console;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {
         "org.abx.service.utils",
         "org.abx.service.console",
         "org.abx.service.spring"})
+@EnableJpaRepositories(value={"org.abx.service.creds.dao"})
+@EntityScan(value = {"org.abx.service.creds.model"})
 public class ABXConsoleEntry {
 
     public static void main(String[] args) {
