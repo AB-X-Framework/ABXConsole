1c8d91056c39c58aa85070d2559f43de790be533
Luis Carlos Lara <l3clara@gmail.com>
2025-01-03 21:32:00 -0600

    test

---
+++ src/main/java/org/abx/service/creds/dao/PrivilegeRepository.java
@@ -0,0 +1,13 @@
+package org.abx.service.creds.dao;
+
+import org.abx.service.creds.model.Privilege;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrivilegeRepository extends JpaRepository<Privilege, Long> {
+
+    Privilege findByName(String name);
+
+    @Override
+    void delete(Privilege privilege);
+
+}
---
+++ src/main/java/org/abx/service/creds/dao/RoleRepository.java
@@ -0,0 +1,13 @@
+package org.abx.service.creds.dao;
+
+import org.abx.service.creds.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByName(String name);
+
+    @Override
+    void delete(Role role);
+
+}
