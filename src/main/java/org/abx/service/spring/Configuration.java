0a1aa2c07306100bb0a4dd50c182ca981795fe7c
Luis Carlos Lara <l3clara@gmail.com>
2025-01-03 22:51:31 -0600

    test

--- src/main/java/org/abx/service/spring/Configuration.java
+++ src/main/java/org/abx/service/spring/Configuration.java
@@ -1,7 +1,6 @@
 package org.abx.service.spring;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
