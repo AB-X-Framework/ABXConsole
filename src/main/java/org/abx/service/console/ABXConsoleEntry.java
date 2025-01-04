15d360b7c5220bef352212bb325959ec76d4a942
Luis Carlos Lara <l3clara@gmail.com>
2025-01-03 21:00:02 -0600

    test

--- src/main/java/org/abx/service/console/ABXConsoleEntry.java
+++ src/main/java/org/abx/service/console/ABXConsoleEntry.java
@@ -3,11 +3,13 @@ package org.abx.service.console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"org.abx.service.utils", "org.abx.service.console"})
+@SpringBootApplication(scanBasePackages = {
+        "org.abx.service.utils",
+        "org.abx.service.console"})
 public class ABXConsoleEntry {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ABXConsoleEntry.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ABXConsoleEntry.class, args);
+    }
 
 }
