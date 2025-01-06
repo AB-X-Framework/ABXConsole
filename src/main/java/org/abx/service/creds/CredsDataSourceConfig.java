18f9e7dfb6009d5e81f08db53fee7230bc2fb332
luis <llara@appvance.com>
2025-01-06 16:45:12 -0600

    test

--- src/main/java/org/abx/service/creds/CredsDataSourceConfig.java
+++ src/main/java/org/abx/service/creds/CredsDataSourceConfig.java
@@ -8,7 +8,6 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -23,36 +22,36 @@ import java.util.Map;
 @EnableTransactionManagement
 @EnableJpaRepositories(
         basePackages = "org.abx.service.creds.dao",
-        entityManagerFactoryRef = "primaryEntityManagerFactory",
-        transactionManagerRef = "primaryTransactionManager"
+        entityManagerFactoryRef = "credsEntityManagerFactory",
+        transactionManagerRef = "credsTransactionManager"
 )
 public class CredsDataSourceConfig {
 
-    @Bean(name = "primaryDataSource")
-    @ConfigurationProperties(prefix = "spring.datasource.primary")
+    @Bean(name = "credsDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.creds")
     public DataSource dataSource() {
         return DataSourceBuilder.create().build();
     }
 
-    @Bean(name = "primaryEntityManagerFactory")
-    @ConfigurationProperties(prefix = "spring.datasource.primary.jpa")
+    @Bean(name = "credsEntityManagerFactory")
+    @ConfigurationProperties(prefix = "spring.datasource.creds.jpa")
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(
             EntityManagerFactoryBuilder builder,
-            @Qualifier("primaryDataSource") DataSource dataSource) {
+            @Qualifier("credsDataSource") DataSource dataSource) {
 
         Map<String, Object> properties = new HashMap<>();
         properties.put("hibernate.hbm2ddl.auto", "update"); // Ensures schema update
         return builder
                 .dataSource(dataSource)
                 .packages("org.abx.service.creds.model") // Your entity package
-                .persistenceUnit("primary")
+                .persistenceUnit("creds")
                 .properties(properties)
                 .build();
     }
 
-    @Bean(name = "primaryTransactionManager")
+    @Bean(name = "credsTransactionManager")
     public PlatformTransactionManager transactionManager(
-            @Qualifier("primaryEntityManagerFactory") EntityManagerFactory entityManagerFactory) {
+            @Qualifier("credsEntityManagerFactory") EntityManagerFactory entityManagerFactory) {
         return new JpaTransactionManager(entityManagerFactory);
     }
 }
