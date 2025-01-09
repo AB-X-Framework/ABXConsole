e3915e62590b42102b8d74c1e0b6afb2285bf5ba
luis <llara@appvance.com>
2025-01-08 18:16:36 -0600

    test

--- src/main/java/org/abx/service/controller/SessionController.java
+++ src/main/java/org/abx/service/controller/SessionController.java
@@ -1,4 +1,22 @@
 package org.abx.service.controller;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class SessionController {
+
+
+    @PostMapping("/login")
+    public String login(final HttpServletRequest request) {
+        try {
+            request.login("abx@abx.com", "abx");
+
+            return "Logged in" + request.getUserPrincipal().getName();
+        } catch (ServletException e) {
+            e.printStackTrace();
+            return "Login failed " + e.getMessage();
+        }
+    }
+
 }
