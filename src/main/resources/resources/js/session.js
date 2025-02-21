dc7fe54258723c3cbef7feceffd05f0c802cae42
luis <llara@appvance.com>
2025-02-21 09:08:16 -0600

    ABX Console

--- src/main/resources/resources/js/session.js
+++ src/main/resources/resources/js/session.js
@@ -9,12 +9,10 @@ $.get({
 
 function parseEntries(entries) {
     function parseEntry(entry) {
-        return '<div style="margin-bottom:5px;">\n         ' +
-            '   <a href="#"\n           ' +
-            '    id="NewDashboard1"\n      ' +
-            '         class="easyui-linkbutton"\n        ' +
-            '       style="width: 100%; text-align: left;"\n        ' +
-            '       onclick="navigateTo(\'NewDashboard.html\')">New Dashboard2</a>\n      ' +
+        return '<div style="margin-bottom:5px;">\n' +
+            ' <a href="#"  class="easyui-linkbutton"\n' +
+            ' style="width: 100%; text-align: left;"\n' +
+            ' onclick="navigateTo(\'NewDashboard.html\')">' + entry.name + '</a>\n      ' +
             '  </div>';
     }
 
@@ -32,8 +30,8 @@ function loadLeftPanel(after) {
             url: "/user/menu",
             success: function (menuData) {
                 $('#Dashboards').append(parseEntries(menuData.dashboards));
-                $('#Projects').append(parseEntries(menuData.dashboards));
-                $('#Executions').append(parseEntries(menuData.dashboards));
+                $('#Projects').append(parseEntries(menuData.projects));
+                $('#Executions').append(parseEntries(menuData.execs));
                 $.parser.parse('#panel'); // Re-initialize EasyUI components
                 if (after !== undefined) {
                     after();
