29c1a9460e11c850b7a07bd04faed34fa298a93b
Luis Carlos <llara@appvance.com>
2025-02-24 17:48:52 -0600

    ABX Console

--- src/main/java/org/abx/console/controller/DashboardsController.java
+++ src/main/java/org/abx/console/controller/DashboardsController.java
@@ -11,8 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,10 +25,26 @@ public class DashboardsController {
     @Autowired
     ServicesClient servicesClient;
 
-    @RequestMapping(value = "/dashboards", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/dashboards", produces = MediaType.APPLICATION_JSON_VALUE)
     @Secured("UseABX")
     public String dashboards(final HttpServletRequest request) throws Exception {
         return getDashboards(request.getUserPrincipal().getName()).toString();
+    }
+
+    @PostMapping(value = "/dashboards", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Secured("UseABX")
+    public String createDashboard(HttpServletRequest request,
+                                @RequestParam String name) throws Exception {
+        String username = request.getUserPrincipal().getName();
+        String token = JWTUtils.generateToken(username, privateKey, 60,
+                List.of("Persistence"));
+        JSONObject result = new JSONObject();
+        try {
+            return result.put("id", servicesClient.post("persistence",
+                    "/persistence/dashboards").jwt(token).addPart("name",name).process().asLong()).toString();
+        }catch (Exception e){
+            return CustomErrorController.errorString("Cannot create "+name+" dashboard."+e.getMessage());
+        }
     }
 
     protected JSONObject getDashboards(String username) throws Exception {
