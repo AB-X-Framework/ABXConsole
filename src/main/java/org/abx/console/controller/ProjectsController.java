44b9e6fa541f44afeb68eb8d61d82997ef14169d
Luis Carlos <l3clara@gmail.com>
2025-03-09 21:56:45 -0600

    ABX Console

--- src/main/java/org/abx/console/controller/ProjectsController.java
+++ src/main/java/org/abx/console/controller/ProjectsController.java
@@ -37,7 +37,7 @@ public class ProjectsController {
         try {
             return result.put("projects", servicesClient.get("persistence",
                     "/persistence/projects").jwt(token).process().asJSONArray());
-        }catch (Exception e){
+        } catch (Exception e) {
             return CustomErrorController.error("Cannot collect user projects.");
         }
     }
@@ -46,28 +46,41 @@ public class ProjectsController {
     @PostMapping(value = "/projects", produces = MediaType.APPLICATION_JSON_VALUE)
     @Secured("UseABX")
     public String createProject(HttpServletRequest request,
-                                  @RequestParam String projectData) throws Exception {
+                                @RequestParam String projectData) throws Exception {
         String username = request.getUserPrincipal().getName();
-        if (true){
-            JSONObject error = CustomErrorController.error("Invalid project");
-            error.put("type", "repos");
-            JSONArray errors = new JSONArray();
 
-            JSONObject jsonRepo = new JSONObject();
-            errors.put(jsonRepo);
-            jsonRepo.put("id", 0);
-            jsonRepo.put("error", "Not valid credentials");
-            error.put("repos",errors);
-            return error.toString();
-        }
         String token = JWTUtils.generateToken(username, privateKey, 60,
-                List.of("Persistence","Repository"));
+                List.of("Persistence", "Repository"));
+        JSONObject jsonData = new JSONObject(projectData);
+        JSONArray repositories = jsonData.getJSONArray("repos");
+        for (int i = 0; i < repositories.length(); ++i) {
+            JSONObject jsonRepoData = repositories.getJSONObject(i);
+            boolean valid = servicesClient.post("repository",
+                            "/repository/validate").jwt(token).
+                    addPart("url", jsonRepoData.getString("url")).
+                    addPart("branch", jsonRepoData.getString("branch")).
+                    addPart("engine", jsonRepoData.getString("engine")).
+                    addPart("creds", jsonRepoData.getJSONObject("creds").toString()).
+                    process().asBoolean();
+            if (!valid) {
+                JSONObject error = CustomErrorController.error("Invalid project");
+                error.put("type", "repos");
+                JSONArray errors = new JSONArray();
+                JSONObject jsonRepo = new JSONObject();
+                errors.put(jsonRepo);
+                jsonRepo.put("id", jsonRepoData.getInt("id"));
+                jsonRepo.put("error", "Invalid credentials");
+                error.put("repos", errors);
+                return error.toString();
+            }
+        }
+
         JSONObject result = new JSONObject();
         try {
             return result.put("id", servicesClient.post("persistence",
-                    "/persistence/projects").jwt(token).addPart("projectData",projectData).process().asLong()).toString();
-        }catch (Exception e){
-            return CustomErrorController.errorString("Cannot create "+new JSONObject(projectData).getString("name")+" project."+e.getMessage());
+                    "/persistence/projects").jwt(token).addPart("projectData", projectData).process().asLong()).toString();
+        } catch (Exception e) {
+            return CustomErrorController.errorString("Cannot create " + new JSONObject(projectData).getString("name") + " project." + e.getMessage());
         }
     }
 
@@ -75,33 +88,33 @@ public class ProjectsController {
     @Secured("Persistence")
     @GetMapping(value = "/projects/{projectId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public String getProject(HttpServletRequest request,
-                               @PathVariable long projectId) throws Exception {
+                             @PathVariable long projectId) throws Exception {
         String username = request.getUserPrincipal().getName();
         String token = JWTUtils.generateToken(username, privateKey, 60,
                 List.of("Persistence"));
         try {
             return servicesClient.get("persistence",
-                    "/persistence/projects/"+projectId).jwt(token).process().asString();
-        }catch (Exception e){
-            return CustomErrorController.errorString("Cannot get dashboard data for project "+projectId);
+                    "/persistence/projects/" + projectId).jwt(token).process().asString();
+        } catch (Exception e) {
+            return CustomErrorController.errorString("Cannot get dashboard data for project " + projectId);
         }
     }
 
     @Secured("Persistence")
     @DeleteMapping(value = "/projects/{projectId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public String deleteProject(HttpServletRequest request,
-                                  @PathVariable long projectId) throws Exception {
+                                @PathVariable long projectId) throws Exception {
         String username = request.getUserPrincipal().getName();
         String token = JWTUtils.generateToken(username, privateKey, 60,
                 List.of("Persistence"));
         JSONObject result = new JSONObject();
         try {
-            boolean success= servicesClient.delete("persistence",
-                    "/persistence/projects/"+projectId).jwt(token).process().asBoolean();
-            result.put("success",success);
+            boolean success = servicesClient.delete("persistence",
+                    "/persistence/projects/" + projectId).jwt(token).process().asBoolean();
+            result.put("success", success);
             return result.toString();
-        }catch (Exception e){
-            return CustomErrorController.errorString("Cannot get project data for "+projectId);
+        } catch (Exception e) {
+            return CustomErrorController.errorString("Cannot get project data for " + projectId);
         }
     }
 
@@ -115,13 +128,13 @@ public class ProjectsController {
                 List.of("Persistence"));
         JSONObject result = new JSONObject();
         try {
-            boolean success= servicesClient.put("persistence",
-                    "/persistence/projects/"+projectId).addPart("projectData",projectData).
+            boolean success = servicesClient.put("persistence",
+                            "/persistence/projects/" + projectId).addPart("projectData", projectData).
                     jwt(token).process().asBoolean();
-            result.put("success",success);
+            result.put("success", success);
             return result.toString();
-        }catch (Exception e){
-            return CustomErrorController.errorString("Cannot get project data for "+projectId);
+        } catch (Exception e) {
+            return CustomErrorController.errorString("Cannot get project data for " + projectId);
         }
     }
 }
